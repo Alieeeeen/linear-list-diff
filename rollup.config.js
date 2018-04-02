9efa93f121ee1e50b9fd815ea723eace802e89ca
Alieeeeen <734943462@qq.com>
2018-04-02 22:03:06 +0800

    uglify build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import uglify from 'rollup-plugin-uglify'
+
 export default {
   input: 'src/list-diff.js',
   output: {
     name: 'diff',
     file: 'dist/list-diff.js',
     format: 'umd'
-  }
+  },
+  plugins: [
+    uglify()
+  ]
 }
